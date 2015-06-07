1f4f3b24a3837e9ae90b3aa451f3ea08b9658528
Floyd Hawkes <floydhawkes@gmail.com>
2015-06-06 21:05:01 -0400

    fix(autocomplete): Fixes issue when md-min-length set to 0 and searchText is an empty string it won't try to set searchText to the displayValue

--- src/components/autocomplete/js/autocompleteController.js
+++ src/components/autocomplete/js/autocompleteController.js
@@ -162,7 +162,7 @@ function MdAutocompleteCtrl ($scope, $element, $mdUtil, $mdConstant, $timeout, $
 
   function selectedItemChange (selectedItem, previousSelectedItem) {
     if (selectedItem) {
-      $scope.searchText = getDisplayValue(selectedItem);
+      $scope.searchText = $scope.searchText === "" ? "" : getDisplayValue(selectedItem);
     }
     if ($scope.itemChange && selectedItem !== previousSelectedItem)
       $scope.itemChange(getItemScope(selectedItem));
